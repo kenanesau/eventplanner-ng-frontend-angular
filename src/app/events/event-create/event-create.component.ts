5e7f82675a46d59ccbe3fade85c9a7be2c949e35
Kenan Esau <kenan.esau@crealogix.com>
2024-01-31 12:22:57 +0100

    added event-create-component

---
+++ src/app/events/event-create/event-create.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {EventFormComponent} from "../event-form/event-form.component";
+import {EventStoreService} from "../shared/event-store.service";
+import {Router} from "@angular/router";
+import {Event} from "../shared/event"
+
+@Component({
+  selector: 'ep-event-create',
+  standalone: true,
+  imports: [CommonModule, EventFormComponent],
+  templateUrl: './event-create.component.html',
+  styleUrls: ['./event-create.component.scss']
+})
+export class EventCreateComponent {
+  constructor(private evService: EventStoreService, private router: Router) {}
+
+  doCreateEvent(changedEvent: Event) {
+    this.evService.create(changedEvent).subscribe(
+      createdEvent => this.router.navigate(['', createdEvent.id]))
+  }
+}
